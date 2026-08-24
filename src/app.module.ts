d74aa3fa3338a3f04be05376af42894c66b16fff
Juan Valentin <juanvalentinanto12@gmail.com>
2026-08-24 00:40:35 -0600

    feat(main) : cambio en enlace de la DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,20 +16,20 @@ import { ReviewsModule } from './reviews/reviews.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 3306),
-        username: configService.get<string>('DB_USERNAME', 'root'),
-        password: configService.get<string>('DB_PASSWORD', ''),
-        database: configService.get<string>('DB_DATABASE', 'trabajos_db'),
-        autoLoadEntities: true,
-        synchronize:
-          configService.get<string>('DB_SYNCHRONIZE', 'false') === 'true',
-      }),
-    }),
+TypeOrmModule.forRootAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    url: configService.get<string>('DATABASE_URL'),
+    autoLoadEntities: true,
+    synchronize:
+      configService.get<string>('DB_SYNCHRONIZE', 'false') === 'true',
+    ssl:
+      configService.get<string>('NODE_ENV') === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
+  }),
+}),
 
     UsersModule,
     AuthModule,
