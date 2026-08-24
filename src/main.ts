96fe2a9aa86a1a93d6d21c15c7b0eb5db5358249
Juan Valentin <juanvalentinanto12@gmail.com>
2026-08-24 00:02:49 -0600

    feat(main) : cambio en el puerto para escuchar peticiones externas

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
